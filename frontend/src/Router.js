df938c04e7bb0fdcccae64ae6a6d31cec3906e0e
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-08-04 15:13:31 -0300

    tirando o elemto routes do controle de rotas

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,27 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-
+import { Route } from "react-router-dom";
 import Cursos from "./pages/Cursos";
 import SignUp from "./pages/SignUp";
 import Sign from "./pages/Sign";
 import Home from "./pages/Home";
 import CheckLogged from "./components/CheckLogged";
 
-
 const Router = () => {
-    return (
-        <Routes>
+  return (
+    <>
+      {/* Remova o componente "Routes" e aninhe as rotas diretamente com "Route" */}
+      <Route element={<CheckLogged />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/cursos" element={<Cursos />} />
+      </Route>
+      <Route path="/signUp" element={<SignUp />} />
+      <Route path="/Sign" element={<Sign />} />
+    </>
+  );
+};
 
-            <Route element={<CheckLogged />}>
-
-                <Route path="/home" element={<Home />} />
-                <Route path="/cursos" element={<Cursos />} />
-
-            </Route>
-
-            <Route path="/signUp" element={<SignUp />} />
-            <Route path="/Sign" element={<Sign />} />
-        </Routes>
-    )
-}
-
-export default Router
+export default Router;
