c6c4b5342ff1418ab1af4e4e0c35de6320b946c3
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-08-04 15:20:28 -0300

    devolvendo o routes

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,22 +1,27 @@
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Cursos from "./pages/Cursos";
 import SignUp from "./pages/SignUp";
 import Sign from "./pages/Sign";
 import Home from "./pages/Home";
 import CheckLogged from "./components/CheckLogged";
 
-const Router = () => {
-  return (
-    <>
-      {/* Remova o componente "Routes" e aninhe as rotas diretamente com "Route" */}
-      <Route element={<CheckLogged />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/cursos" element={<Cursos />} />
-      </Route>
-      <Route path="/signUp" element={<SignUp />} />
-      <Route path="/Sign" element={<Sign />} />
-    </>
-  );
-};
 
-export default Router;
+const Router = () => {
+    return (
+        <Routes>
+
+            <Route element={<CheckLogged />}>
+
+                <Route path="/home" element={<Home />} />
+                <Route path="/cursos" element={<Cursos />} />
+
+            </Route>
+
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/Sign" element={<Sign />} />
+        </Routes>
+    )
+}
+
+export default Router
