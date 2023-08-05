7686ae5ab839b5b4db920c8f32349a687b2994cc
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-08-05 12:22:01 -0300

    add video e arrumadno CORS

--- api/config/cors.php
+++ api/config/cors.php
@@ -1,6 +1,6 @@
 <?php
-// Habilitar CORS para somente undertakehigh.com.br/teste
-header("Access-Control-Allow-Origin: *");
+// Habilitar CORS somente para 'https://rotiv.netlify.app'
+header("Access-Control-Allow-Origin: https://rotiv.netlify.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Max-Age: 86400"); // Tempo de cache para opções pré-voo (em segundos)
--- frontend/src/components/EventoCard.jsx
+++ frontend/src/components/EventoCard.jsx
@@ -1,22 +1,37 @@
-
-import { Card, CardContent, Typography } from '@mui/material';
+import React from 'react';
+import { Card, CardContent, Typography, Box } from '@mui/material';
 
 const EventoCard = ({ evento }) => {
-    return (
-        <Card>
-            <CardContent>
-                <Typography variant="h5" component="h1">
-                    {evento.titulo}
-                </Typography>
-                <Typography variant="subtitle1" component="h2">
-                    {evento.descricao}
-                </Typography>
-                <Typography variant="subtitle2" component="h2">
-                    {evento.nome}
-                </Typography>
-            </CardContent>
-        </Card>
-    );
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h5" component="h1">
+          {evento.titulo}
+        </Typography>
+        <Typography variant="subtitle1" component="h2">
+          {evento.descricao}
+        </Typography>
+        <Box
+          width="100%"
+          height="0"
+          paddingTop="56.25%" // 16:9 aspect ratio for responsive video
+          position="relative"
+        >
+          <iframe
+            src={evento.aula}
+            title="Vídeo do YouTube"
+            width="100%"
+            height="100%"
+            style={{ position: 'absolute', top: 0, left: 0 }}
+            allowFullScreen
+          />
+        </Box>
+        <Typography variant="subtitle2" component="h2">
+          {evento.nome}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
-export default EventoCard
+export default EventoCard;
