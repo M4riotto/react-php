d93e366597e4bde378f778bc227ce5f0496ab722
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-08-05 12:38:33 -0300

    apagando um curso

---
+++ api/apagar.php
@@ -0,0 +1,23 @@
+<?php
+
+    require ('config/cors.php');
+    require ('./database/database.php');
+
+    try {
+        $stmt = $connect->prepare("DELETE FROM cursos WHERE id = 3;
+        ");
+
+        $stmt->execute();
+
+        $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $result["success"]["message"] = "Curso listada com sucesso"; //criamos o array para devolver o resultado do insert numa mensagem de sucesso.
+
+        $result["data"] = $eventos; //criamos o array para devolver o resultado do insert com os dados inseridos.
+
+        header('Content-Type: Text/json'); //para ser enviado no formato json.
+        echo json_encode($result); //exibir o resultado.
+    } catch (PDOException $erro) {
+        echo "falha ao cadastrar" . $erro->getMessage();
+    }
+?>
