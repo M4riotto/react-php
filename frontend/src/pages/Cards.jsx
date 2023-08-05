e1b3c185e1253af74664221f2d8df1b2bc30a653
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-08-04 22:55:59 -0300

    apagando o que nao foi usado

--- frontend/src/pages/Cards.jsx
+++ frontend/src/pages/Cards.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { TextField, Button, Container, Typography, Box, Grid } from '@mui/material';
+// import { TextField, Button, Container, Typography, Box, Grid } from '@mui/material';
 
 import { API_SERVER } from '../config'
 
-import useAuthStore from '../store/useAuthStore';
-import Logout from '../components/Logout';
-import { useNavigate } from 'react-router-dom';
+// import useAuthStore from '../store/useAuthStore';
+// import Logout from '../components/Logout';
+// import { useNavigate } from 'react-router-dom';
 
 const Cards = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const [eventos, setEventos] = useState(false);
 
