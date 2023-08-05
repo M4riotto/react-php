7c120f8a8256203d85eb6c051a7f88ddb246f399
Vitor Moreira <110418519+M4riotto@users.noreply.github.com>
2023-08-04 22:49:37 -0300

    criando o select do card

---
+++ api/selectCurso.php
@@ -0,0 +1,21 @@
+<?php
+
+    require ('config/cors.php');
+    require ('./database/database.php');
+
+    try {
+        $stmt = $connect->prepare("SELECT * FROM cursos");
+        $stmt->execute();
+
+        $eventos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $result["success"]["message"] = "Curso listada com sucesso"; //criamos o array para devolver o resultado do insert numa mensagem de sucesso.
+
+        $result["data"] = $eventos; //criamos o array para devolver o resultado do insert com os dados inseridos.
+
+        header('Content-Type: Text/json'); //para ser enviado no formato json.
+        echo json_encode($result); //exibir o resultado.
+    } catch (PDOException $erro) {
+        echo "falha ao cadastrar" . $erro->getMessage();
+    }
+?>
